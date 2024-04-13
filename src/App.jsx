d25fef3955e3967fbc2904760c89083fbcff90c3
KatWine <kwinegardner@pursuit.org>
2024-04-13 19:28:33 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 function App () {
+  const [score,setScore]=useState(0);
+  const [increment,setIncrement]=useState(1)
+  const increaseScore=()=> setScore(prevscore =>prevscore + increment);
+  const increaseIncrement =()=>{
+    if(score>10){
+      setScore(prevscore => prevscore-10);
+      setIncrement(prevIncrement => prevIncrement +1)
+    }else{
+      alert('Not Enough Points')
+    }
+  }
     return (
       <main>
-        <p>React State Lab</p>
+       <h1>Current Score {score}</h1>
+       <button onClick={()=>increaseScore()}> +{increment} </button><br />
+       <button onClick={()=> increaseIncrement()}>Pay 10 pts to change +{increment} to +{increment + 1}</button>
       </main>
     );
 }
